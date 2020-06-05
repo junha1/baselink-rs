ff0959afb2035f32f8f498cad9422c5209b56c77
Junha Yang0 <junha@codechain.io>
2020-06-05 11:29:14 +0900

    Stream wip

--- basesandbox/src/ipc/domain_socket2.rs
+++ basesandbox/src/ipc/domain_socket2.rs
@@ -1,40 +1,41 @@
 use super::*;
-use parking_lot::Mutex;
-use std::cell::RefCell;
-use std::io::{Read, Write};
-use mio::net::{UnixStream, UnixListener};
-use std::sync::Arc;
-use std::marker::PhantomData;
+use mio::net::{UnixListener, UnixStream};
 use mio::{Events, Interest, Poll, Token};
-use mio::net::UdpSocket;
+use parking_lot::Mutex;
+use std::io::{Read, Write};
+use std::sync::Arc;
 const POLL_TOKEN: Token = Token(0);
+use crossbeam::channel::{self, Receiver, Sender};
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread;
-use crossbeam::channel::{Receiver, Sender, self};
 
-fn send_routine(queue: Receiver<Vec<u8>>, write_signal: Receiver<Result<(), ()>>, socket: Arc<Mutex<UnixStream>>) -> Result<(), ()> {
-    let mut send_helper= |buf: &[u8]| {
+fn send_routine(
+    queue: Receiver<Vec<u8>>,
+    write_signal: Receiver<Result<(), ()>>,
+    socket: Arc<Mutex<UnixStream>>,
+) -> Result<(), ()> {
+    let send_helper = |buf: &[u8]| {
         let mut sent = 0;
-        let mut events = Events::with_capacity(1);
         while sent < buf.len() {
             match socket.lock().write(buf) {
                 Ok(x) => sent += x,
                 Err(e) => {
                     match e.kind() {
                         // spurious wakeup
-                        std::io::ErrorKind::WouldBlock => write_signal.recv().map_err(|_| ())?, 
+                        std::io::ErrorKind::WouldBlock => write_signal.recv().unwrap().map_err(|_| ())?,
                         _ => panic!("Failed to send"),
                     }
-                },
+                }
             }
         }
         assert_eq!(sent, buf.len());
         Ok(())
     };
-    
+
     loop {
-        let x = queue.recv().unwrap();
+        let x = queue.recv().map_err(|_| ())?;
         if x.is_empty() {
-            return Err(());
+            return Err(())
         }
         let size: [u8; 8] = x.len().to_be_bytes();
         send_helper(&size)?;
@@ -42,128 +43,160 @@ fn send_routine(queue: Receiver<Vec<u8>>, write_signal: Receiver<Result<(), ()>>
     }
 }
 
-fn recv_routine(queue: Sender<Vec<u8>>, read_signal: Receiver<Result<(), ()>>, socket: Arc<Mutex<UnixStream>>) -> Result<(), ()>  {
-    let mut recv_helper = |buf: &mut [u8]| {
+fn recv_routine(
+    queue: Sender<Vec<u8>>,
+    read_signal: Receiver<Result<(), ()>>,
+    socket: Arc<Mutex<UnixStream>>,
+) -> Result<(), ()> {
+    let recv_helper = |buf: &mut [u8]| {
         let r = socket.lock().read_exact(buf);
         match r {
             Ok(_) => Ok(true),
             Err(e) => match e.kind() {
-                std::io::ErrorKind::UnexpectedEof => Err(RecvError::Termination),
+                std::io::ErrorKind::UnexpectedEof => Err(()),
                 std::io::ErrorKind::WouldBlock => Ok(false), // spurious wakeup
                 e => panic!(e),
             },
         }
     };
-    let mut events = Events::with_capacity(1);
-
-    let size;
     loop {
-        read_signal.recv().unwrap()?;
-        let mut size_buf = [0 as u8; 8];
-        if recv_helper(&mut size_buf, &mut *guard)? {
-            size = usize::from_be_bytes(size_buf);
-            break;
+        let size;
+        loop {
+            read_signal.recv().unwrap()?;
+            let mut size_buf = [0 as u8; 8];
+            if recv_helper(&mut size_buf)? {
+                size = usize::from_be_bytes(size_buf);
+                break
+            }
         }
-    }
+        assert_ne!(size, 0);
+        // TODO: avoid useless zero fill
+        let mut result: Vec<u8> = vec![0; size];
 
-    let mut result: Vec<u8> = Vec::with_capacity(size);
-    // TODO: avoid useless initialization
-    result.resize(size, 0);
-    if size == 0 {
-        panic!()
-    }
-
-    loop {
-        let mut guard = self.socket.lock();
-        if recv_helper(&mut result[0..], &mut *guard)? {
-            break;
+        loop {
+            if recv_helper(&mut result[0..])? {
+                break
+            }
+            // we wait later this time, since the preceeding polling might be upto this recv.
+            read_signal.recv().unwrap()?;
         }
-        // we wait later this time, since the preceeding polling might be upto this recv.
-        self.poll.lock().poll(&mut events, timeout).map_err(|_| RecvError::TimeOut)?;
-        assert_eq!(events.iter().nth(0).unwrap().token(), POLL_TOKEN, "Invalid socket event");
+        queue.send(result).map_err(|_| ())?;
     }
-    Ok(result)
 }
 
-fn poll_routine(write_signal: Sender<()>, recv_signal: Sender<()>, mut poll: Poll) {
+fn poll_routine(
+    exit_flag: Arc<AtomicBool>,
+    write_signal: Sender<Result<(), ()>>,
+    recv_signal: Sender<Result<(), ()>>,
+    mut poll: Poll,
+) {
     // TODO: does the capacity matter?
     let mut events = Events::with_capacity(100);
     loop {
         poll.poll(&mut events, None).unwrap();
         for event in events.iter() {
-            assert_eq!(events.iter().nth(0).unwrap().token(), POLL_TOKEN, "Invalid socket event");
+            assert_eq!(events.iter().next().unwrap().token(), POLL_TOKEN, "Invalid socket event");
+
+            // If it is going to exit, it's ok to fail to send signal (some spurious signals come)
+            let exit = if exit_flag.load(Ordering::Relaxed) {
+                Ok(())
+            } else {
+                Err(())
+            };
+
             // termintation
-            if event.is_error() { 
-                return;
+            if event.is_write_closed() || event.is_read_closed() {
+                write_signal.send(Err(())).ok();
+                recv_signal.send(Err(())).ok();
+                return
             }
             if event.is_writable() {
-                write_signal.send(()).unwrap();
+                write_signal.send(Ok(())).map_err(|_| ()).or(exit).unwrap();
             }
             if event.is_readable() {
-                recv_signal.send(()).unwrap();
+                recv_signal.send(Ok(())).map_err(|_| ()).or(exit).unwrap();
             }
         }
     }
 }
 
 struct SocketInternal {
-    send_thread: Option<thread::JoinHandle<()>>,
-    poll_thread: Option<thread::JoinHandle<()>>,
+    _send_thread: Option<thread::JoinHandle<()>>,
+    _recv_thread: Option<thread::JoinHandle<()>>,
+    _poll_thread: Option<thread::JoinHandle<()>>,
+    exit_flag: Arc<AtomicBool>,
+    socket: Arc<Mutex<UnixStream>>,
 }
 
-impl SocketInternal {
-    fn create(mut socket: UnixStream) -> (DomainSocketSend, DomainSocketRecv, Arc<Self>) {
-        let mut poll = Poll::new().unwrap();
-        poll.registry()
-        .register(&mut socket, POLL_TOKEN, Interest::WRITABLE.add(Interest::READABLE)).unwrap();
-
-        let socket = Arc::new(Mutex::new(socket));
-        let (send_queue_send, send_queue_recv) = channel::unbounded();
-        let (write_signal_send, write_signal_recv) = channel::unbounded();
-        let (read_signal_send, read_signal_recv) = channel::unbounded();
-
-        let socket_ = socket.clone();
-        let send_thread = Some(thread::spawn(|| {
-            send_routine(send_queue_recv, write_signal_recv, socket_);
-        }));
-        
-        let poll_thread = Some(thread::spawn(|| {
-            poll_routine(write_signal_send, read_signal_send, poll);
-        }));
-
-        (
-            DomainSocketSend{queue: send_queue_send},
-            DomainSocketRecv{
-
-            },
-        SocketInternal {
-            send_thread,
-            poll_thread
-        })
-    }
-}
-
-impl Drop for SocketInternal{
+impl Drop for SocketInternal {
     fn drop(&mut self) {
-        self.send_thread.take().unwrap().join().unwrap();
-        self.poll_thread.take().unwrap().join().unwrap();
+        self.exit_flag.store(true, Ordering::Relaxed);
+        self.socket.lock().shutdown(std::net::Shutdown::Both).unwrap();
+        self._send_thread.take().unwrap().join().unwrap();
+        self._recv_thread.take().unwrap().join().unwrap();
+        self._poll_thread.take().unwrap().join().unwrap();
     }
 }
 
+fn create(mut socket: UnixStream) -> (DomainSocketSend, DomainSocketRecv) {
+    let poll = Poll::new().unwrap();
+    poll.registry().register(&mut socket, POLL_TOKEN, Interest::WRITABLE.add(Interest::READABLE)).unwrap();
+
+    let socket = Arc::new(Mutex::new(socket));
+    let (send_queue_send, send_queue_recv) = channel::unbounded();
+    let (recv_queue_send, recv_queue_recv) = channel::unbounded();
+    let (write_signal_send, write_signal_recv) = channel::unbounded();
+    let (read_signal_send, read_signal_recv) = channel::unbounded();
+
+    let socket_ = socket.clone();
+    let _send_thread = Some(thread::spawn(|| {
+        send_routine(send_queue_recv, write_signal_recv, socket_).unwrap_err();
+    }));
+    let socket_ = socket.clone();
+    let _recv_thread = Some(thread::spawn(|| {
+        let terminate_send = recv_queue_send.clone();
+        recv_routine(recv_queue_send, read_signal_recv, socket_).unwrap_err();
+        terminate_send.send(Vec::new()).ok(); // doesn't matter even if it fails
+    }));
+
+    let exit_flag = Arc::new(AtomicBool::new(false));
+    let exit_flag_ = exit_flag.clone();
+    let _poll_thread = Some(thread::spawn(|| poll_routine(exit_flag_, write_signal_send, read_signal_send, poll)));
+
+    let socket_internal = Arc::new(SocketInternal {
+        _send_thread,
+        _recv_thread,
+        _poll_thread,
+        exit_flag,
+        socket,
+    });
+
+    (
+        DomainSocketSend {
+            queue: send_queue_send,
+            _socket: socket_internal.clone(),
+        },
+        DomainSocketRecv {
+            queue: recv_queue_recv,
+            socket: socket_internal,
+        },
+    )
+}
 
 pub struct DomainSocketSend {
-    queue: Sender<Vec<u8>>
+    queue: Sender<Vec<u8>>,
+    _socket: Arc<SocketInternal>,
 }
 
 impl IpcSend for DomainSocketSend {
     fn send(&self, data: &[u8]) {
-
+        self.queue.send(data.to_vec()).unwrap();
     }
 }
 
 pub struct DomainSocketRecv {
-    socket: Arc<Mutex<UnixStream>>,
-    _prevent_sync: PhantomData<std::cell::Cell<()>>
+    queue: Receiver<Vec<u8>>,
+    socket: Arc<SocketInternal>,
 }
 
 impl IpcRecv for DomainSocketRecv {
@@ -171,48 +204,15 @@ impl IpcRecv for DomainSocketRecv {
 
     /// Note that DomainSocketRecv is !Sync, so this is guaranteed to be mutual exclusive.
     fn recv(&self, timeout: Option<std::time::Duration>) -> Result<Vec<u8>, RecvError> {
-        fn recv_helper(buf: &mut [u8], stream: &mut UnixStream) -> Result<bool, RecvError> {
-            let r = stream.read_exact(buf);
-            match r {
-                Ok(_) => Ok(true),
-                Err(e) => match e.kind() {
-                    std::io::ErrorKind::UnexpectedEof => Err(RecvError::Termination),
-                    std::io::ErrorKind::WouldBlock => Ok(false), // spurious wakeup
-                    e => panic!(e),
-                },
-            }
+        let x = if let Some(t) = timeout {
+            self.queue.recv_timeout(t).map_err(|_| RecvError::TimeOut)?
+        } else {
+            self.queue.recv().unwrap()
+        };
+        if x.is_empty() {
+            return Err(RecvError::Termination)
         }
-        let mut events = Events::with_capacity(1);
-
-        let size;
-        loop {
-            self.poll.lock().poll(&mut events, timeout).map_err(|_| RecvError::TimeOut)?;
-            assert_eq!(events.iter().nth(0).unwrap().token(), POLL_TOKEN, "Invalid socket event");
-            let mut guard = self.socket.lock();
-            let mut size_buf = [0 as u8; 8];
-            if recv_helper(&mut size_buf, &mut *guard)? {
-                size = usize::from_be_bytes(size_buf);
-                break;
-            }
-        }
-
-        let mut result: Vec<u8> = Vec::with_capacity(size);
-        // TODO: avoid useless initialization
-        result.resize(size, 0);
-        if size == 0 {
-            panic!()
-        }
-
-        loop {
-            let mut guard = self.socket.lock();
-            if recv_helper(&mut result[0..], &mut *guard)? {
-                break;
-            }
-            // we wait later this time, since the preceeding polling might be upto this recv.
-            self.poll.lock().poll(&mut events, timeout).map_err(|_| RecvError::TimeOut)?;
-            assert_eq!(events.iter().nth(0).unwrap().token(), POLL_TOKEN, "Invalid socket event");
-        }
-        Ok(result)
+        Ok(x)
     }
 
     fn create_terminator(&self) -> Self::Terminator {
@@ -220,11 +220,12 @@ impl IpcRecv for DomainSocketRecv {
     }
 }
 
-pub struct Terminator(Arc<Mutex<UnixStream>>);
+pub struct Terminator(Arc<SocketInternal>);
 
 impl Terminate for Terminator {
     fn terminate(&self) {
-        if let Err(e) = (self.0).lock().shutdown(std::net::Shutdown::Both) {
+        self.0.exit_flag.store(true, Ordering::Relaxed);
+        if let Err(e) = (self.0).socket.lock().shutdown(std::net::Shutdown::Both) {
             assert_eq!(e.kind(), std::io::ErrorKind::NotConnected);
         }
     }
@@ -256,73 +257,46 @@ impl IpcRecv for DomainSocket {
 impl Ipc for DomainSocket {
     fn arguments_for_both_ends() -> (Vec<u8>, Vec<u8>) {
         let path_gen = || format!("{}/{}", std::env::temp_dir().to_str().unwrap(), generate_random_name());
-        let server_send = path_gen();
-        let client_send = path_gen();
-        let server_recv = path_gen();
-        let client_recv = path_gen();
-        (
-            serde_cbor::to_vec(&(true, &server_send, &server_recv)).unwrap(),
-            serde_cbor::to_vec(&(false, &server_send, &server_recv)).unwrap(),
-        )
+        let address = path_gen();
+        (serde_cbor::to_vec(&(true, &address)).unwrap(), serde_cbor::to_vec(&(false, &address)).unwrap())
     }
 
     type SendHalf = DomainSocketSend;
     type RecvHalf = DomainSocketRecv;
 
     fn new(data: Vec<u8>) -> Self {
-        let (am_i_server, address_send, address_recv): (bool, String, String) = serde_cbor::from_slice(&data).unwrap();
-
-        let connect = |is_server: bool, address: String| {
-            if am_i_server {
-                let listener = UnixListener::bind(&address).unwrap();
-                (|| {
-                    for _ in 0..100 {
-                        if let Ok(stream) = listener.accept() {
-                            return stream
-                        }
-                        std::thread::sleep(std::time::Duration::from_millis(10));
-                    }
-                    panic!("Failed to establish domain socket within a timeout")
-                })().0
-            } else {
-                (|| {
-                    for _ in 0..100 {
-                        if let Ok(stream) = UnixStream::connect(&address) {
-                            return stream
-                        }
-                        std::thread::sleep(std::time::Duration::from_millis(10));
-                    }
-                    panic!("Failed to establish domain socket within a timeout")
-                })()
-            }
-        };
+        let (am_i_server, address): (bool, String) = serde_cbor::from_slice(&data).unwrap();
 
         // We use spinning for the connection establishment
-        let mut stream_send = connect(am_i_server, address_send); 
+        let stream = if am_i_server {
+            let listener = UnixListener::bind(&address).unwrap();
+            (|| {
+                for _ in 0..100 {
+                    if let Ok(stream) = listener.accept() {
+                        return stream
+                    }
+                    std::thread::sleep(std::time::Duration::from_millis(10));
+                }
+                panic!("Failed to establish domain socket within a timeout")
+            })()
+            .0
+        } else {
+            (|| {
+                for _ in 0..100 {
+                    if let Ok(stream) = UnixStream::connect(&address) {
+                        return stream
+                    }
+                    std::thread::sleep(std::time::Duration::from_millis(10));
+                }
+                panic!("Failed to establish domain socket within a timeout")
+            })()
+        };
 
-        let mut stream_recv = connect(am_i_server, address_recv); 
-
-
-        let mut poll1 = Poll::new().unwrap();
-        poll1.registry()
-        .register(&mut stream_send, POLL_TOKEN, Interest::WRITABLE).unwrap();
-
-        let mut poll2 = Poll::new().unwrap();
-        poll2.registry()
-        .register(&mut stream_recv, POLL_TOKEN, Interest::READABLE).unwrap();
-
+        let (send, recv) = create(stream);
 
         DomainSocket {
-            send: DomainSocketSend {
-                socket: Arc::new(Mutex::new(stream_send)),
-                poll: Mutex::new(poll1),
-                _prevent_sync: PhantomData
-            },
-            recv: DomainSocketRecv {
-                socket: Arc::new(Mutex::new(stream_recv)),
-                poll: Mutex::new(poll2),
-                _prevent_sync: PhantomData
-            },
+            send,
+            recv,
         }
     }
 
@@ -339,8 +313,6 @@ fn f123() {
     let s1 = DomainSocket::new(a1);
     let s2 = t.join().unwrap();
 
-    println!("HAHAHAHA");
-
     let huge_data = {
         let mut v = Vec::new();
         for i in 0..300 {
@@ -350,9 +322,7 @@ fn f123() {
     };
 
     s2.send(&huge_data);
-
     let r = s1.recv(None).unwrap();
-
     s1.send(&huge_data);
     let r = s2.recv(None).unwrap();
 }
